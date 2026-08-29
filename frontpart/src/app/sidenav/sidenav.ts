18c86876f8837701dd8dab42f7ba458441e06f09
rueduweb <rhu.sab@outlook.fr>
2026-08-29 17:53:46 +0200

    visual improvements to the viewport and tweaks to the side navigation

--- frontpart/src/app/sidenav/sidenav.ts
+++ frontpart/src/app/sidenav/sidenav.ts
@@ -16,38 +16,83 @@ export class Sidenav implements OnInit {
 
   public onToggleSidenav = output<SidenavToggle>();
   public screenWidth = signal<number>(0);
+  // Desktop
   public collapsed = signal<boolean>(false);
+
+  // Mobile
+  public mobileMenuOpen = signal<boolean>(false);
   public navData = signal(navbarData);
 
   protected readonly auth = inject(Auth);
   private readonly router = inject(Router);
 
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
+  @HostListener('window:resize')
+  onResize(): void {
     this.screenWidth.set(window.innerWidth);
-    if(this.screenWidth() <= 768) {
-      this.collapsed.set(false);
-      this.onToggleSidenav.emit({screenWidth: this.screenWidth(), collapsed: this.collapsed()});
+
+    if (this.screenWidth() >= 768) {
+      this.mobileMenuOpen.set(false);
     }
+
+    this.onToggleSidenav.emit({
+      screenWidth: this.screenWidth(),
+      collapsed: this.collapsed()
+    });
+  }
+
+  toggleMobileMenu(): void {
+    this.mobileMenuOpen.update(value => !value);
   }
 
   ngOnInit(): void {
     this.screenWidth.set(window.innerWidth);
+
+    this.onToggleSidenav.emit({
+      screenWidth: this.screenWidth(),
+      collapsed: this.collapsed()
+    });
   }
 
+
   toggleCollapse(): void {
-    this.collapsed.set(!this.collapsed());
-    this.onToggleSidenav.emit({screenWidth: this.screenWidth(), collapsed: this.collapsed()});
+
+    if (this.isMobile()) {
+      this.toggleMobileMenu();
+      return;
+    }
+
+    this.collapsed.update(value => !value);
+
+    this.onToggleSidenav.emit({
+      screenWidth: this.screenWidth(),
+      collapsed: this.collapsed()
+    });
   }
 
   closeSidenav(): void {
+    if (this.isMobile()) {
+      this.mobileMenuOpen.set(false);
+      return;
+    }
+
     this.collapsed.set(false);
-    this.onToggleSidenav.emit({screenWidth: this.screenWidth(), collapsed: this.collapsed()});
+
+    this.onToggleSidenav.emit({
+      screenWidth: this.screenWidth(),
+      collapsed: this.collapsed()
+    });
   }
+
 
   onLogout(): void {
     this.auth.logout();
     this.router.navigate(['/home']);
   }
+
+  // private methods
+  private isMobile(): boolean {
+    return this.screenWidth() < 768;
+  }
+
 
 }
