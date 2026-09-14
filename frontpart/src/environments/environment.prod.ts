8e82a2a6ac8b866d116cb5fbf58048a31c0885a7
speedy237 <97912735+speedy237@users.noreply.github.com>
2026-09-14 21:11:29 +0200

    Update environment.prod.ts

--- frontpart/src/environments/environment.prod.ts
+++ frontpart/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  API_BASE_URL: 'http://localhost:3000' // distant API address not yet specified
+  API_BASE_URL: 'http://api.plateforme-ifc.com' // distant API address not yet specified
 }
